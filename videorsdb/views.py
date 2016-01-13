c11522a658c76ff68b6b88089e12b3cd96a0d7d1
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-01-12 22:04:16 -0300

    Vista index agregado

--- videorsdb/views.py
+++ videorsdb/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+  bienvenido = 'Bienvenido'
+  return render(request, 'index.html', {'bienvenido':bienvenido})
