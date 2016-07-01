e8db1bdf9b625ceb78693ec60c79a4889b6c3f5f
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-07-01 10:13:05 -0300

    modelo subir video agregado

--- videorsdb/models.py
+++ videorsdb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class DownloadVideo(models.Model):
+    id_video = models.AutoField(primary_key=True)
+    cod_video = models.CharField(max_length=11)
+    nombre_video = models.CharField(max_length=100)
+    video_file = models.FileField(upload_to='video/', max_length=200)
+    id_u = models.ForeignKey(User)
