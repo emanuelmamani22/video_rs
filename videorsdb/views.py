c18846520f9a0000f81aa9fe8f24bec43036770e
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-07-02 13:08:16 -0300

    funcion de vista login y logout

--- videorsdb/views.py
+++ videorsdb/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from .forms import RegistroUserForm
+from django.contrib import messages
+from .forms import RegistroUserForm, login_user
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
@@ -26,3 +29,29 @@ def registro_user(request):
   else :
     form = RegistroUserForm()
     return render(request, 'registro.html', {'form':form})
+
+def login_views(request):
+  message = None
+  if request.method == 'POST':
+    form = login_user(request.POST)
+    if form.is_valid():
+      username = request.POST['username']
+      password = request.POST['password']
+      user = authenticate(username=username, password=password)
+      if user is not None:
+        if user.is_active:
+          login(request, user)
+          return HttpResponseRedirect('/')
+        else :
+          message = 'Tu usuario esta inactivo'
+      else :
+          message = "Nombre de usuario y/o password incorrecto"
+    else :
+      return render(request, 'login.html', {'message':message,'form':form})
+  else :
+    form = login_user()
+  return render(request, 'login.html', {'message':message, 'form':form})
+
+def logout_views(request): 
+    logout(request)    
+    return redirect('index')
