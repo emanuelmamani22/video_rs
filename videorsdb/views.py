756f22c53b15154b16664a520d2d1c54ad056d86
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-07-02 15:30:32 -0300

    vista subir video

--- videorsdb/views.py
+++ videorsdb/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import RegistroUserForm, login_user
+from .forms import RegistroUserForm, login_user, subirvideo
 from django.contrib.auth import authenticate, login, logout
+from .models import DownloadVideo
+from .funciones import calcular_codigo
 # Create your views here.
 
 def index(request):
@@ -55,3 +57,24 @@ def login_views(request):
 def logout_views(request): 
     logout(request)    
     return redirect('index')
+
+def up_video(request):
+    if request.user.is_authenticated():
+      if request.method == 'POST':
+        form = subirvideo(request.POST, request.FILES)
+        if form.is_valid():
+          namevideo = request.POST['nombre_video']
+          video_archivo = request.FILES['archivo_video']
+          usuario = request.user
+          cod = calcular_codigo()
+          video = DownloadVideo(cod_video=cod, nombre_video=namevideo, video_file=video_archivo, id_u=usuario)
+          video.save()
+
+          return HttpResponseRedirect('/')
+        else :
+          return render(request, 'up_video.html', {'form':form})
+      else :
+        form = subirvideo()
+    else :
+      return HttpResponseRedirect('/login')
+    return render(request, 'up_video.html', {'form':form})
