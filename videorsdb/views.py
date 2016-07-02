87aed9a8429227e7054f46edf10304a50b4c879e
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-07-01 23:13:47 -0300

    Funcion de vista Registro de usuario

--- videorsdb/views.py
+++ videorsdb/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .forms import RegistroUserForm
 # Create your views here.
 
 def index(request):
   bienvenido = 'Bienvenido'
   return render(request, 'index.html', {'bienvenido':bienvenido})
+
+def registro_user(request):
+  if request.method == 'POST'
+    form = RegistroUserForm(request.POST)
+    if form.is_valid():
+      namefirst = request.POST['namefirst']
+      namelast = request.POST['namelast']
+      username = request.POST['username']
+      email = request.Post['email']
+      password = request.Post['password']
+     
+      insert = User.objects.create_user(password=password, username=username, email=email, first_name=namefirst, last_name=namelast)
+      insert.save()
+     
+      return redirect('index')
+    else :
+      return render(request, 'login.html', {'form': form})
+  else :
+    form = RegistroUserForm(request.Post)
+    return render(request, 'login.hmtl', {'form':form})
