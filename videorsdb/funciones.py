f10d6af5076d12561c3209c735188d67aaf7e855
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-07-03 13:51:08 -0300

    funcion que devuelve valores para formulario desplegable

--- videorsdb/funciones.py
+++ videorsdb/funciones.py
@@ -1,6 +1,5 @@
 import random
-from .models import UploadVideo
-
+from .models import UploadVideo, Tagvideo
 
 def calcular_codigo():
     a=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','_','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','-','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
@@ -14,3 +13,13 @@ def calcular_codigo():
        calcular_codigo()
     
     return b
+
+
+def choice_tag():
+  from django.db import connection
+  cursor = connection.cursor()
+  cursor.execute("""SELECT videorsdb_tagvideo.id_tag, videorsdb_tagvideo.nombre_tag FROM public.videorsdb_tagvideo""")
+  row = cursor.fetchall()
+  #choice_field=((str(row[0][0]), row[0][1],), (str(row[1][0], row[1][1],),(str(row[2][0]), row[2][1],),(str(row[3][0]), row[3][1],))
+  CHOICES = (('1', row[0][1],), ('2', row[1][1],),('2', row[2][1],),('2', row[3][1],),('2', row[4][1],),('2', row[5][1],),('2', row[6][1],),('2', row[7][1],),('2', row[8][1],),('2', row[9][1],))
+  return CHOICES
