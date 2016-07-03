bd6fca748157cfaf0267a59878e315c279806d7b
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-07-03 10:50:33 -0300

    nombre de modelo cambiado

--- videorsdb/models.py
+++ videorsdb/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class DownloadVideo(models.Model):
+class UploadVideo(models.Model):
     id_video = models.AutoField(primary_key=True)
     cod_video = models.CharField(max_length=11)
     nombre_video = models.CharField(max_length=100)
