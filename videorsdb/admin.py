746ea989bb27c2ffdd0846b3520127878c02189c
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-08-30 12:01:19 -0300

    agregar admin canal

--- videorsdb/admin.py
+++ videorsdb/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from videorsdb.models import UploadVideo
+from videorsdb.models import UploadVideo, Canal
 
 # Register your models here.
 admin.site.register(UploadVideo)
+admin.site.register(Canal)
