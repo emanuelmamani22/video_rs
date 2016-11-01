0860b0c972fe1da5cd08815ee5d2dd145a167c23
Emmanuel Mamani <emanuel_media15@hotmail.com>
2016-11-01 15:29:49 -0300

    cambios menores

--- videorsdb/views.py
+++ videorsdb/views.py
@@ -35,11 +35,14 @@ def sugerencias(request):
 def index(request):
 	r=random.randrange(1,10)
 	q = UploadVideo.objects.filter(id_tag=r).order_by('?')[:4]
-	name_q = q[0].id_tag.nombre_tag 
+	try :
+		name_q = q[0].id_tag.nombre_tag
+	except IndexError:
+		name_q = ''
 	if request.user.is_authenticated():
 		user = request.user
 		ureco = Likeanddislike.objects.filter(id_u=user.id, megusta=True).order_by('?')[:4]
-		return render(request, 'index.html', {'q':q, 'hola':'hola','name_q':name_q, 'ureco':ureco}, context_instance=RequestContext(request, processors=[canal_user, subcripciones_user]))
+		return render(request, 'index.html', {'q':q,'name_q':name_q, 'ureco':ureco}, context_instance=RequestContext(request, processors=[canal_user, subcripciones_user]))
 	return render(request, 'index.html', {'q':q,'name_q':name_q, 'hola':'hola'},)
 
 def registro_user(request):
